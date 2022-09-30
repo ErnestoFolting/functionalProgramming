8aeae18664d201c80764a8f1a48251aff5065569
Danil <danilsmy.edu@gmail.com>
2022-10-01 00:12:22 +0300

    Add correct dotNetTask project and realization

---
+++ Lab1/dotNetTask/dotNetTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotNetTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = functions.singleton(1);
+            Console.WriteLine("Singleton:");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Null: {0}", functions.Null(lst));
+            lst = functions.snoc(lst, 2);
+            Console.WriteLine("Snoc");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Length: {0}", functions.Length(lst));
+        }
+    }
+}
---
+++ Lab1/dotNetTask/dotNetTask/functions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetTask
+{
+    public static class functions
+    {
+        public static List<T> singleton<T>(T el)
+        {
+            return new() { el };
+        } 
+        
+        public static bool Null<T>(List<T> elements)
+        {
+            return (Length(elements) == 0);
+        }
+        public static List<T> snoc <T> (List<T> lst, T el)
+        {
+            int previousSize = Length(lst);
+            T[] newLst = new T[previousSize + 1];   
+            for(int i = 0; i < previousSize; i++)
+            {
+                newLst[i] = lst[i];
+            }
+            newLst[previousSize] = el;
+            return newLst.ToList();
+        }
+        public static int Length<T>(List<T> elements)
+        {
+            int count = 0;
+            foreach (T element in elements)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
