a3ead56a997e4f2fc8d9ec58ca722eb55e331e63
Danil <danilsmy.edu@gmail.com>
2022-10-18 14:43:20 +0300

    Add zip and unzip functions

--- Lab2/src/Lab2.purs
+++ Lab2/src/Lab2.purs
@@ -7,6 +7,9 @@ import Data.List (length)
 import Data.Maybe(Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Tuple (Tuple(..))
+import Data.Tuple (fst)
+import Data.Tuple (snd)
 
 {-findIndex
 Знайдіть перший індекс елементу списку, для якого виконується предикат.
@@ -36,9 +39,40 @@ findLastIndex pred lst = case(findIndex pred (reverse lst)) of
     Just n -> Just((length lst - n - 1))
     Nothing -> Nothing
 
-
+{-zip
+zip візьме 2 списки та заархівує їх в один список, де кожен елемент вихідного списку є Tuple який містить перший елемент List у першій позиції та другий List у другій позиції
+-}
+zip :: forall a b. List a -> List b -> List(Tuple a b)
+zip lst1 lst2 = 
+    let 
+        go :: forall a b. List a -> List b -> List(Tuple a b) -> List(Tuple a b)
+        go lst1 lst2 acc = case lst1 of
+            (x:xs)->
+                case lst2 of
+                    (y:ys) ->  go xs ys ((Tuple x y) : acc)
+                    _ -> acc
+            _ -> acc
+    in 
+         (go lst1 lst2 Nil)
+{-unzip 
+unzip є зворотним до zip. Ви повинні мати можливість взяти результат zip і використати його, щоб розархівувати та отримати ваші вихідні списки назад, припускаючи, що ви почали зі списків однакового розміру.
+-} 
+unzip :: forall a b. List (Tuple a b) -> Tuple (List a) (List b)
+unzip lst = 
+    let 
+        go :: forall a b. List (Tuple a b) -> List a -> List b -> Tuple (List a) (List b)
+        go lst acc1 acc2 = case lst of 
+            (x:xs) -> case fst x of
+                val1 -> case snd x of
+                    val2 -> go xs (val1:acc1) (val2:acc2)
+            _ -> Tuple acc1 acc2 
+    in
+        (go lst Nil Nil)
 
 test :: Effect Unit
 test = do 
     log $ show $ findIndex predicate (2:(3:(1:(2:Nil))))
-    log $ show $ findLastIndex predicate (2:(3:(1:(4:Nil))))
+    log $ show $ findLastIndex predicate (2:(3:(1:(4:Nil))))   
+    log $ show $ zip (2:(3:(1:(2:Nil)))) (5:(2:(3:(1:(2:Nil)))))
+    log $ show $ unzip(zip (2:(3:(1:(2:Nil)))) (5:(2:(3:(1:(2:Nil))))))
+    
