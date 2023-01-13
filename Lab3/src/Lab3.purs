8a8671209e14d2b8854c83e27ba164e3c92405bd
Danil <danilsmy.edu@gmail.com>
2023-01-13 20:18:33 +0200

    Add subtask3

--- Lab3/src/Lab3.purs
+++ Lab3/src/Lab3.purs
@@ -1,6 +1,6 @@
 module Lab3 where
 
-import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=))
+import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=), (<>))
 import Data.Eq (class Eq)
 import Data.Ord (class Ord, Ordering(..), compare)
 import Data.Show (class Show)
@@ -22,22 +22,26 @@ instance ordMaybe :: Ord a => Ord (Maybe a) where
   compare (Just _) (Nothing) = GT
   compare (Nothing) (Just _) = LT
 
+instance showMaybe :: Show a => Show (Maybe a) where
+  show (Just a) = "(Just " <> show a <> ")."
+  show Nothing = "Nothing."
+
 test :: Effect Unit
 test = do
   log $ show $ "Example"
-  log $ show $ Just 5 == Just 5 -- COMPILER ERROR!! 
+  log $ show $ Just 5 == Just 5 
   log $ show $ Just 5 == Just 2
   log $ show $ Just 5 == Nothing
   log $ show $ Nothing == Just 5
   log $ show $ Nothing == (Nothing :: Maybe Unit)
   log $ show $ "Subtask1 and subtask2"
   log "------------------"
-  log $ show $ Just 1 < Just 5 -- COMPILER ERROR!!
+  log $ show $ Just 1 < Just 5 
   log $ show $ Just 5 <= Just 5
   log $ show $ Just 5 > Just 10
   log $ show $ Just 10 >= Just 10
   log $ show $ Just 99 > Nothing
   log $ show $ Just 99 < Nothing
   log $ show $ "Subtask3"
-  -- log $ show $ Just "abc"
-  -- log $ show $ (Nothing :: Maybe Unit)
+  log $ show $ Just "abc"
+  log $ show $ (Nothing :: Maybe Unit)
